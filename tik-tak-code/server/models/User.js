7a2a99b6fa1184c700829a1abe7a193b2402045e
Mohamed <abdi2k24@gmail.com>
2023-02-14 19:33:19 -0600

    updated js

--- tik-tak-code/server/models/User.js
+++ tik-tak-code/server/models/User.js
@@ -19,5 +19,5 @@ const userSchema = new mongoose.Schema({
     timestamps:true
 })
 
-const User =mongoose.model('User',userSchema)
+const User = mongoose.model('User',userSchema)
 module.exports= User;
